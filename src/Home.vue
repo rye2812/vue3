e7712b08e5516dac944255a729dc6ea9c53f65f6
rey2812 <ajaxst@163.com>
2023-10-17 07:40:05 +0800

    增加主页面文件

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,20 @@
 <template>
-    <div @click="change()">我是子组件1</div>
+    <div @click="change()">我是子组件</div>
     {{ money }} --- {{ car }}
 </template>
 
 <script setup>
 import {computed,inject} from 'vue'
-const money = inject('money')
+const money = inject("money")
 const props = defineProps({
     money: Number,
     car: String
 })
 
-const emit = defineEmits(['changeMoney'])
+const emit = defineEmits(['changeMoney']);
 
 const change = () => {
-    emit('changeMoney',15)
+    emit('changeMoney',20)
 }
 
 </script>
